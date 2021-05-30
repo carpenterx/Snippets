246630a29fc07cf33e2185220c214e42ff0ce023
Sorin <jordache_sor@yahoo.com>
2021-05-30 11:52:47 +0300

    added snippet class (fixes #2)

---
+++ Snippets/Models/Snippet.cs
@@ -0,0 +1,11 @@
+﻿namespace Snippets.Models
+{
+    class Snippet
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Prerequisites { get; set; }
+        public string Code { get; set; }
+        public int UseCount { get; set; }
+    }
+}
