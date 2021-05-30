58f39c8893bb80d4880effadd68648c38812e3c5
Sorin <jordache_sor@yahoo.com>
2021-05-30 13:50:56 +0300

    added code to parse the data into snippets (fixes #3)

--- Snippets/MainWindow.xaml.cs
+++ Snippets/MainWindow.xaml.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Snippets.Models;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -22,6 +24,15 @@ namespace Snippets
     public partial class MainWindow : Window
     {
         private static readonly string SNIPPETS_PATH = @"C:\Users\jorda\Desktop\snippets.txt";
+        private static readonly string SNIPPET_SEPARATOR = "======================  SNIPPET  =======================";
+        private static readonly string NAME_PREFIX = "//  Name         :  ";
+        private static readonly string DESCRIPTION_PREFIX = "//  Description  :  ";
+        private static readonly string USE_COUNT_PREFIX = "//  Used         :  ";
+        private static readonly string PREREQUISITES = "===================  [PREREQUISITES]  ==================";
+        private static readonly string CODE_START = "====================  [CODE START]  ====================";
+        private static readonly string CODE_END = "=====================  [CODE END]  =====================";
+        private List<Snippet> snippetsList = new();
+
         public MainWindow()
         {
             InitializeComponent();
@@ -31,7 +42,100 @@ namespace Snippets
 
         private void ReadSnippetsFile(string path)
         {
-            snippetTxt.Text = File.ReadAllText(path);
+            List<string> snippets = File.ReadAllText(path).Split(SNIPPET_SEPARATOR).ToList().Skip(1).ToList();
+            
+            foreach (string snippetText in snippets)
+            {
+                ParseSnippetText(snippetText);
+            }
+
+            DisplaySnippet(snippetsList[1]);
+        }
+
+        private void ParseSnippetText(string text)
+        {
+            snippetsList.Add(new Snippet
+            {
+                Title = ExtractName(text),
+                Description = ExtractDescription(text),
+                UseCount = ExtractUseCount(text),
+                Prerequisites = ExtractPrerequisites(text),
+                Code = ExtractCode(text)
+            });
+        }
+
+        private string ExtractName(string text)
+        {
+            string name = "";
+            string pattern = $"{NAME_PREFIX}(.+)\r";
+            Match m = Regex.Match(text, pattern, RegexOptions.Multiline);
+            if (m.Success)
+            {
+                name = m.Groups[1].Value;
+            }
+            return name;
+        }
+
+        private string ExtractDescription(string text)
+        {
+            string description = "";
+            string pattern = $"{DESCRIPTION_PREFIX}(.+)\r";
+            Match m = Regex.Match(text, pattern, RegexOptions.Multiline);
+            if (m.Success)
+            {
+                description = m.Groups[1].Value;
+            }
+            return description;
+        }
+
+        private string ExtractUseCount(string text)
+        {
+            string useCount = "";
+            string pattern = $"{USE_COUNT_PREFIX}(.+)\r";
+            Match m = Regex.Match(text, pattern, RegexOptions.Multiline);
+            if (m.Success)
+            {
+                useCount = m.Groups[1].Value;
+            }
+            return useCount;
+        }
+
+        private string ExtractPrerequisites(string text)
+        {
+            string prerequisites = "";
+            int startIndex = text.IndexOf(PREREQUISITES) + PREREQUISITES.Length + 2;
+            if(startIndex > -1)
+            {
+                int endIndex = text.IndexOf(CODE_START) - 2;
+                prerequisites = text.Substring(startIndex, endIndex - startIndex);
+            }
+            return prerequisites;
+        }
+
+        private string ExtractCode(string text)
+        {
+            string code;
+            int startIndex = text.IndexOf(CODE_START) + CODE_START.Length + 2;
+            int endIndex = text.IndexOf(CODE_END) - 2;
+            code = text[startIndex..endIndex];
+            return code;
+        }
+
+        private void DisplaySnippet(Snippet snippet)
+        {
+            StringBuilder snippetStringBuilder = new();
+            snippetStringBuilder.AppendLine("===========================");
+            snippetStringBuilder.AppendLine(snippet.Title);
+            snippetStringBuilder.AppendLine("===========================");
+            snippetStringBuilder.AppendLine(snippet.Description);
+            snippetStringBuilder.AppendLine("===========================");
+            snippetStringBuilder.AppendLine(snippet.UseCount);
+            snippetStringBuilder.AppendLine("===========================");
+            snippetStringBuilder.AppendLine(snippet.Prerequisites);
+            snippetStringBuilder.AppendLine("===========================");
+            snippetStringBuilder.AppendLine(snippet.Code);
+            snippetStringBuilder.AppendLine("===========================");
+            snippetTxt.Text = snippetStringBuilder.ToString();
         }
     }
 }
